4142e343d20c541a986af35190d8c374797c18a1
Kamil Grabczak <grabczakkamil@gmail.com>
2018-05-06 10:40:22 +0200

    zadanie8536 zrobione z pomocą Adama

--- js/index.js
+++ js/index.js
@@ -1,24 +1,23 @@
 $(function() {
     'use strict';
 
-$("#first").click(function(){
-    
+$("#first").click(function() {
 var parentElement = document.getElementById("main");
 
-var theFirstChild = parentElement.firstChild;
+var theFirstChild = parentElement.firstElementChild;
 
-var theLastChild = parentElement.lastChild;
+var theLastChild = parentElement.lastElementChild;
 
 parentElement.insertBefore(theLastChild, theFirstChild);   
 
-})    
+});    
     
 
-$("#second").click(function(){
+$("#second").click(function() {
     
 var parentElement = document.getElementById("main");
 
-var theFirstChild = parentElement.firstChild;
+var theFirstChild = parentElement.firstElementChild;
   
 parentElement.appendChild(theFirstChild);
     
